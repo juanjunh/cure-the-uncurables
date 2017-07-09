f0a1ba3429cc7ad3d2c625bf7aad01d58c48922f
JanielHNY <janielhny@janielhnyde-air.gateway>
2017-07-09 20:20:44 +0930

    Initial Commit

---
+++ curetheuncurables/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  curetheuncurables
+//
+//  Created by JanielHNY on 2017/7/9.
+//  Copyright © 2017年 JanielHNY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
